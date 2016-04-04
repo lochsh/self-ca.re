ee9509171c5edb65a0f0b1d78af2ff89b8496a48
Hannah McLaughlin <hannah.mclaughlin@paconsulting.com>
2016-04-04 12:50:43 +0100

    Atoned for younger self's sins, and PEP8-ified selcare.py

--- selfcare.py
+++ selfcare.py
@@ -1,45 +1,46 @@
-#!/usr/bin/env/python
-
 import random as rnd
 
-"""
-Function to count lines in a file
-"""
-def count_lines(fname):
-    with open(fname) as f:
-        for i, l in enumerate(f):
+
+def count_lines(file_name):
+    """Return number of lines in a file"""
+    with open(file_name) as f:
+        for i, _ in enumerate(f):
             pass
     return i + 1
 
-"""
-Read file line by line into list
-"""
-def read_lines(fname):
+
+def read_lines(file_name):
+    """Read file line by line into list, return list"""
     words = []
-    with open(fname) as f:
+    with open(file_name) as f:
         for line in f:
             words.append(line.rstrip())
     return words
 
-"""
-Make inspirational message ;o
-"""
+
 def selfcare():
-    #Count how many adjectives and nouns
-    A = count_lines('adjectives.txt')
-    N = count_lines('nouns.txt')
+    """
+    Return inspirational message ;o
 
-    #Randomly pick one adjective and one noun
-    a = rnd.randint(0,A-1)
-    n = rnd.randint(0,N-1)
+    Reads a list of adjectives from adjectives.txt, and a list of nouns from
+    nouns.txt.  A random entry from each is picked, and the two are combined to
+    create an inspirational message:
 
-    #Read adjectives and nouns
+    'You are a <adjective> <noun>'
+    """
+    # Count how many adjectives and nouns
+    num_adj = count_lines('adjectives.txt')
+    num_noun = count_lines('nouns.txt')
+
+    # Randomly pick one adjective and one noun
+    ind_adj = rnd.randint(0, num_adj - 1)
+    ind_noun = rnd.randint(0, num_noun - 1)
+
+    # Read adjectives and nouns
     adj = read_lines('adjectives.txt')
     nouns = read_lines('nouns.txt')
+    return 'You are a%s %s' % (adj[ind_adj], nouns[ind_noun])
 
-    #;o  quick and dirty line breaks as I haven't got vertical alignment to work in CSS yet :'(
-    message = 'You are a' + adj[a] + ' ' + nouns[n]
-    return message
 
 def main():
     print selfcare()
