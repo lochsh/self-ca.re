7bf763a0995c47bcf071e80f17aa729187bdff03
Hannah McLaughlin <hannah.mclaughlin@paconsulting.com>
2016-04-04 12:52:39 +0100

    updated selfcare_app.py to be in line with style guide

--- selfcare_app.py
+++ selfcare_app.py
@@ -1,14 +1,12 @@
-#!/usr/bin/env python
-
-from flask import Flask, render_template
+from flask import Flask
 from selfcare import selfcare
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def sc():
-    x = selfcare()
-    return x
+    return selfcare()
 
 if __name__ == '__main__':
     app.run()
