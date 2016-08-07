894345f9b3d0b1b7861f16cd25edae052d4dac0a
Hannah McLaughlin <the.banannah@gmail.com>
2016-08-07 18:19:21 +0100

    Repenting for past sins.  The voice of experience that I can now speak with: 1. use .format instead of % 2. it's not C! no need to count the number of words and randomly choose and index.  Use the stdlib provided to you

--- selfcare.py
+++ selfcare.py
@@ -1,14 +1,6 @@
 import random
 
 
-def count_lines(file_name):
-    """Return number of lines in a file"""
-    with open(file_name) as f:
-        for i, _ in enumerate(f):
-            pass
-    return i + 1
-
-
 def read_lines(file_name):
     """Return list where each entry is a line from file"""
     with open(file_name) as f:
@@ -25,15 +17,10 @@ def selfcare():
 
     'You are a <adjective> <noun>'
     """
-    num_adj = count_lines('adjectives.txt')
-    num_noun = count_lines('nouns.txt')
-
-    ind_adj = random.randint(0, num_adj - 1)
-    ind_noun = random.randint(0, num_noun - 1)
-
-    adj = read_lines('adjectives.txt')
+    adjs = read_lines('adjectives.txt')
     nouns = read_lines('nouns.txt')
-    return 'You are a%s %s' % (adj[ind_adj], nouns[ind_noun])
+    return 'You are a{0} {1}'.format(random.choice(adjs),
+                                     random.choice(nouns))
 
 
 def main():
